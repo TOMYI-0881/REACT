dbeb3fc5b0525082a3dc391f39bd09cb22bda6c7
TOMYI-0881 <gthomasenrique0883@gmail.com>
2026-03-27 14:50:56 -0500

    nuevo

--- src/09-useContext/context/UseContext.tsx
+++ src/09-useContext/context/UseContext.tsx
@@ -1,9 +1,35 @@
-import { useState } from "react";
+import { createContext, useContext, useState, type PropsWithChildren } from "react";
+import type { User } from "../data/user-mock.data";
 
-interface UserContexProvider {
-  children: React.ReactNode;
+// interface UserContexProvider {
+//   children: React.ReactNode;
+// }
+
+type AuthStatus = "checking" | "authenticated"| "not-authenticated";
+
+interface UserCotextProps {
+  //state
+  authStatus: AuthStatus;
+  user: User | null;
+
+  //methods
+  login: (userId: number) => boolean;
+  logout: () => void;
 }
 
-export const UseContextProvider = ({ children }: UserContexProvider) => {
-  return <>{children}</>;
+//creamos el canal del contexto
+export const UserContext = createContext({} as UserCotextProps)
+  
+export const UseContextProvider = ({ children }: PropsWithChildren) => {
+  
+  const handleLogin = (id: number) => true
+
+  const handleLogout = () => console.log("logout")
+
+  return <UserContext value={{
+    authStatus:"checking",
+    user: null,
+    login: handleLogin,
+    logout: handleLogout
+  }}>{children}</UserContext>;
 };
